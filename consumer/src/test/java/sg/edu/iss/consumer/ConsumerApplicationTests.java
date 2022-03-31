bbc8b4aad24570c4a23a0d20c60cbd8e2c51ca28
Suria R Asai <suria.r.asai@gmail.com>
2022-03-31 15:59:58 +0800

    Simple Spring Kafka Consumer

---
+++ consumer/src/test/java/sg/edu/iss/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.iss.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
